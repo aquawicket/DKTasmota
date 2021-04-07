124701d3629cae0b2490cc8b6623988d90b62330
dkuser <dkuser@digitalknob.com>
2021-04-07 07:38:54 -0700

    commit from git

--- DKPlugins/DKTasmota/DKMqtt.js
+++ DKPlugins/DKTasmota/DKMqtt.js
@@ -9,28 +9,25 @@ var reconnectTimeout = 2000;
 var host = "192.168.1.78";
 var port = 9001;
 
-function onMQTTConnect() {
+function DKMqtt_OnMqttConnect() {
     // Once a connection has been made, make a subscription and send a message.
-    console.log("Mqtt Connected ");
+    console.log("Mqtt Connected");
     //mqtt.subscribe("sensor1");
     message = new Paho.MQTT.Message("Hello World");
     message.destinationName = "sensor1";
     mqtt.send(message);
 }
 
-function MQTTconnect() {
-    console.log("connecting to " + host + " " + port);
+function DKMqtt_Connect() {
+    dkconsole.log("connecting to " + host + " " + port);
     var x = Math.floor(Math.random() * 10000);
     var cname = "orderform-" + x;
     mqtt = new Paho.MQTT.Client(host,port,cname);
     //document.write("connecting to "+ host);
     var options = {
-
         timeout: 3,
-        onSuccess: onMQTTConnect,
-
+        onSuccess: DKMqtt_OnMqttConnect,
     };
 
     mqtt.connect(options);
-    //connect
 }
