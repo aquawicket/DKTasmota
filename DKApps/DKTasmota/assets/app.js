02443ae6ba3c1c03451790dab880e2e905aaa31d
dkuser <dkuser@digitalknob.com>
2021-04-20 07:48:35 -0700

    commit from git

--- DKApps/DKTasmota/assets/app.js
+++ DKApps/DKTasmota/assets/app.js
@@ -1,7 +1,7 @@
 console.log("loading app.js");
-CPP_DKDuktape_Create("DKWindow");
-CPP_DKDuktape_Create("DKRml");
-CPP_DKDuktape_Create("DKSDLText");
+CPP_DK_Create("DKWindow");
+CPP_DK_Create("DKRml");
+CPP_DK_Create("DKSDLText");
 
 location.href = CPP_DKAssets_LocalAssets()+"/index.html";
 //location.href = CPP_DKAssets_LocalAssets()+"/DKTasmota/index.html";
