9588e27786ad31484ca8777666c8e2f9ef0cc3a2
dkuser <dkuser@digitalknob.com>
2021-05-07 12:06:08 -0700

    commit from git

--- DKPlugins/DKTasmota/Automation.js
+++ DKPlugins/DKTasmota/Automation.js
@@ -22,6 +22,7 @@ let VegTentHeat;
 
 function Automate() {
 
+    // Device alias accessors
     !ATentCo2 && (ATentCo2 = dk.tasmota.device("A Tent Co2"));
     !ATentTemp && (ATentTemp = dk.tasmota.device("A Tent Temp"));
     !ATentExhaust && (ATentExhaust = dk.tasmota.device("A Tent Exhaust Fan"));
@@ -42,22 +43,45 @@ function Automate() {
     !VegTentWater && (VegTentWater = dk.tasmota.device("Veg Tent Water Walls"));
     !VegTentHeat && (VegTentHeat = dk.tasmota.device("Veg Tent Heater"));
 
+    // Presets
+    const veg_preset = {
+        temperatureTarget: 77,
+        temperatureZone: 20,
+        humidityTarget: 50,
+        humidityZone: 20
+    }
+    const bloom_preset = {
+        temperatureTarget: 77,
+        temperatureZone: 20,
+        humidityTarget: 40,
+        humidityZone: 20
+    }
+    const co2_preset = {
+        temperatureTarget: 85,
+        temperatureZone: 20,
+        humidityTarget: 50,
+        humidityZone: 20
+    }
+    const dry_preset = {
+        temperatureTarget: 65,
+        temperatureZone: 20,
+        humidityTarget: 45,
+        humidityZone: 40
+    }
+    const empty_preset = {
+        temperatureTarget: 100,
+        temperatureZone: 20,
+        humidityTarget: 50,
+        humidityZone: 40
+    }
+
     ////////////////////////////////////////////////////////////
-    //A Tent Presets
+    //A Tent
     if (ATentTemp && ATentTemp.user) {
-        if (ATentCo2 && ATentCo2.user.automate) {
-            //When using Co2, temperature should average 85 degrees
-            ATentTemp.user.temperatureTarget = 85;
-            ATentTemp.user.temperatureZone = 20;
-            ATentTemp.user.humidityTarget = 50;
-            ATentTemp.user.humidityZone = 20;
-        } else {
-            //When NOT using Co2, temperature should be 77 degrees
-            ATentTemp.user.temperatureTarget = 77;
-            ATentTemp.user.temperatureZone = 20;
-            ATentTemp.user.humidityTarget = 50;
-            ATentTemp.user.humidityZone = 20;
-        }
+        if (ATentCo2 && ATentCo2.user.automate)
+            Object.assign(ATentTemp.user, co2_preset);
+        else
+            Object.assign(ATentTemp.user, veg_preset);
 
         //A Tent Alarms
         if (ATentTemp.user.temperature < (ATentTemp.user.temperatureTarget - ATentTemp.user.temperatureZone))
@@ -72,7 +96,7 @@ function Automate() {
         //A Tent Co2
         if (ATentCo2 && ATentCo2.user.automate) {
             let power = false;
-            (ATentTemp.user.temperature < (ATentTemp.user.temperatureTarget-1)) && (power = true);
+            (ATentTemp.user.temperature < (ATentTemp.user.temperatureTarget - 1)) && (power = true);
             power && (ATentCo2.user.power === "OFF") && console.log("A Tent Co2 Fan ON", "green");
             !power && (ATentCo2.user.power === "ON") && console.log("A Tent Co2 Fan OFF", "yellow");
             dk.sendRequest("http://" + ATentCo2.ip + "/cm?cmnd=POWER%20" + power, myapp.updateScreen);
@@ -84,8 +108,8 @@ function Automate() {
             (ATentTemp.user.temperature > ATentTemp.user.temperatureTarget) && (power = true);
             (ATentTemp.user.humidity > ATentTemp.user.humidityTarget) && (power = true);
             //(ATentTemp.user.temperature < (ATentTemp.user.temperatureTarget - 10)) && (power = false);
-            (dk.clock.time < dk.clock.sunrise+.30) && (power = false);
-            (dk.clock.time > (dk.clock.sunset+1)) && (power = false);
+            (dk.clock.time < dk.clock.sunrise + .30) && (power = false);
+            (dk.clock.time > (dk.clock.sunset + 1)) && (power = false);
             (ATentTemp.user.humidity > 90) && (power = true);
             power && (ATentExhaust.user.power === "OFF") && console.log("A Tent Exhaust Fan ON", "green");
             !power && (ATentExhaust.user.power === "ON") && console.log("A Tent Exhaust Fan OFF", "yellow");
@@ -98,7 +122,7 @@ function Automate() {
             (ATentTemp.user.humidity < ATentTemp.user.humidityTarget) && (power = true);
             (ATentTemp.user.temperature > ATentTemp.user.temperatureTarget) && (power = true);
             (dk.clock.time < dk.clock.sunrise) && (power = false);
-            (dk.clock.time > (dk.clock.sunset-1)) && (power = false);
+            (dk.clock.time > (dk.clock.sunset - 1)) && (power = false);
             power && (ATentWater.user.power === "OFF") && console.log("A Tent Water walls ON", "green");
             !power && (ATentWater.user.power === "ON") && console.log("A Tent Water walls OFF", "yellow");
             dk.sendRequest("http://" + ATentWater.ip + "/cm?cmnd=POWER%20" + power, myapp.updateScreen);
@@ -115,29 +139,14 @@ function Automate() {
     }
 
     ////////////////////////////////////////////////////////////
-    //B Tent Presets
+    //B Tent
     if (BTentTemp && BTentTemp.user) {
-        if (BTentCo2 && BTentCo2.user.automate) {
-            //When using Co2, temperature should average 85 degrees
-            BTentTemp.user.temperatureTarget = 85;
-            BTentTemp.user.temperatureZone = 20;
-            BTentTemp.user.humidityTarget = 50;
-            BTentTemp.user.humidityZone = 20;
-        } else {
-            //When NOT using Co2, temperature should be 77 degrees
-            BTentTemp.user.temperatureTarget = 70;
-            BTentTemp.user.temperatureZone = 20;
-            BTentTemp.user.humidityTarget = 65;
-            BTentTemp.user.humidityZone = 20;
-        }
-        //drying room
-        
-        BTentTemp.user.temperatureTarget = 77;
-        BTentTemp.user.temperatureZone = 10;
-        BTentTemp.user.humidityTarget = 45;
-        BTentTemp.user.humidityZone = 10;
-       
-        
+        if (BTentCo2 && BTentCo2.user.automate)
+            Object.assign(BTentTemp.user, co2_preset);
+        else
+            Object.assign(BTentTemp.user, veg_preset);
+        Object.assign(BTentTemp.user, dry_preset)
+
         //B Tent Alarms
         if (BTentTemp.user.temperature < (BTentTemp.user.temperatureTarget - BTentTemp.user.temperatureZone))
             console.log("!!! B TENT TEMPERATURE BELOW MINIMUM(" + (BTentTemp.user.temperatureTarget - BTentTemp.user.temperatureZone) + "&#176;F) @" + BTentTemp.user.temperature + "&#176;F !!!", "red");
@@ -151,7 +160,7 @@ function Automate() {
         //B Tent Co2
         if (BTentCo2 && BTentCo2.user.automate) {
             let power = false;
-            (BTentTemp.user.temperature < (BTentTemp.user.temperatureTarget-1)) && (power = true);
+            (BTentTemp.user.temperature < (BTentTemp.user.temperatureTarget - 1)) && (power = true);
             power && (BTentCo2.user.power === "OFF") && console.log("B Tent Co2 ON", "green");
             !power && (BTentCo2.user.power === "ON") && console.log("B Tent Co2 OFF", "yellow");
             dk.sendRequest("http://" + BTentCo2.ip + "/cm?cmnd=POWER%20" + power, myapp.updateScreen);
@@ -163,8 +172,8 @@ function Automate() {
             (BTentTemp.user.temperature > BTentTemp.user.temperatureTarget) && (power = true);
             (BTentTemp.user.humidity > BTentTemp.user.humidityTarget) && (power = true);
             //(BTentTemp.user.temperature < (BTentTemp.user.temperatureTarget - 10)) && (power = false);
-            (dk.clock.time < dk.clock.sunrise+.30) && (power = false);
-            (dk.clock.time > (dk.clock.sunset+1)) && (power = false);
+            (dk.clock.time < dk.clock.sunrise + .30) && (power = false);
+            (dk.clock.time > (dk.clock.sunset + 1)) && (power = false);
             (BTentTemp.user.humidity > 90) && (power = true);
             power && (BTentExhaust.user.power === "OFF") && console.log("B Tent Exhaust Fan ON", "green");
             !power && (BTentExhaust.user.power === "ON") && console.log("B Tent Exhaust Fan OFF", "yellow");
@@ -177,7 +186,7 @@ function Automate() {
             (BTentTemp.user.humidity < BTentTemp.user.humidityTarget) && (power = true);
             (BTentTemp.user.temperature < BTentTemp.user.temperatureTarget) && (power = false);
             (dk.clock.time < dk.clock.sunrise) && (power = false);
-            (dk.clock.time > (dk.clock.sunset-1)) && (power = false);
+            (dk.clock.time > (dk.clock.sunset - 1)) && (power = false);
             power && (BTentWater.user.power === "OFF") && console.log("B Tent Water walls ON", "green");
             !power && (BTentWater.user.power === "ON") && console.log("B Tent Water walls OFF", "yellow");
             dk.sendRequest("http://" + BTentWater.ip + "/cm?cmnd=POWER%20" + power, myapp.updateScreen);
@@ -196,10 +205,7 @@ function Automate() {
     ////////////////////////////////////////////////////////////
     //Shed Presets
     if (ShedTemp && ShedTemp.user) {
-        ShedTemp.user.temperatureTarget = 75;
-        ShedTemp.user.temperatureZone = 10;
-        ShedTemp.user.humidityTarget = 50;
-        ShedTemp.user.humidityZone = 20;
+        Object.assign(ShedTemp.user, veg_preset);
 
         //Shed Alarms
         if (ShedTemp.user.temperature < (ShedTemp.user.temperatureTarget - ShedTemp.user.temperatureZone))
@@ -215,19 +221,11 @@ function Automate() {
     ////////////////////////////////////////////////////////////
     //Veg Tent Presets
     if (VegTentTemp && VegTentTemp.user) {
-        if (VegTentCo2 && VegTentCo2.user.automate) {
-            //When using Co2, temperature should average 85 degrees
-            VegTentTemp.user.temperatureTarget = 85;
-            VegTentTemp.user.temperatureZone = 20;
-            VegTentTemp.user.humidityTarget = 50;
-            VegTentTemp.user.humidityZone = 20;
-        } else {
-            //When NOT using Co2, temperature should be 77 degrees
-            VegTentTemp.user.temperatureTarget = 77;
-            VegTentTemp.user.temperatureZone = 20;
-            VegTentTemp.user.humidityTarget = 50;
-            VegTentTemp.user.humidityZone = 20;
-        }
+        if (VegTentCo2 && VegTentCo2.user.automate)
+            Object.assign(VegTentTemp.user, co2_preset);
+        else
+            Object.assign(VegTentTemp.user, veg_preset);
+        Object.assign(VegTentTemp.user, empty_preset);
 
         //Veg Tent Alarms
         if (VegTentTemp.user.temperature < (VegTentTemp.user.temperatureTarget - VegTentTemp.user.temperatureZone))
@@ -242,7 +240,7 @@ function Automate() {
         //Veg Tent Co2
         if (VegTentCo2 && VegTentCo2.user.automate) {
             let power = false;
-            (VegTentTemp.user.temperature < (VegTentTemp.user.temperatureTarget-1)) && (power = true);
+            (VegTentTemp.user.temperature < (VegTentTemp.user.temperatureTarget - 1)) && (power = true);
             power && (VegTentCo2.user.power === "OFF") && console.log("Veg Tent Co2 ON", "green");
             !power && (VegTentCo2.user.power === "ON") && console.log("Veg Tent Co2 OFF", "yellow");
             dk.sendRequest("http://" + VegTentCo2.ip + "/cm?cmnd=POWER%20" + power, myapp.updateScreen);
@@ -255,8 +253,8 @@ function Automate() {
             (VegTentTemp.user.humidity > VegTentTemp.user.humidityTarget) && (power = true);
             //(VegTentTemp.user.temperature < (VegTentTemp.user.temperatureTarget - 10)) && (power = false);
             //(VegTentTemp.user.humidity < VegTentTemp.user.humidityTarget) && (power = false);
-            (dk.clock.time < dk.clock.sunrise+.30) && (power = false);
-            (dk.clock.time > (dk.clock.sunset+1)) && (power = false);
+            (dk.clock.time < dk.clock.sunrise + .30) && (power = false);
+            (dk.clock.time > (dk.clock.sunset + 1)) && (power = false);
             //(VegTentTemp.user.humidity > 91) && (power = true);
             power && (VegTentExhaust.user.power === "OFF") && console.log("Veg Tent Exhaust Fan ON", "green");
             !power && (VegTentExhaust.user.power === "ON") && console.log("Veg Tent Exhaust Fan OFF", "yellow");
@@ -271,7 +269,7 @@ function Automate() {
             (VegTentTemp.user.humidity > VegTentTemp.user.humidityTarget) && (power = false);
             (VegTentTemp.user.temperature < VegTentTemp.user.temperatureTarget) && (power = false);
             (dk.clock.time < dk.clock.sunrise) && (power = false);
-            (dk.clock.time > (dk.clock.sunset-1)) && (power = false);
+            (dk.clock.time > (dk.clock.sunset - 1)) && (power = false);
             power && (VegTentWater.user.power === "OFF") && console.log("Veg Tent Water Walls ON", "green");
             !power && (VegTentWater.user.power === "ON") && console.log("Veg Tent Water Walls OFF", "yellow");
             dk.sendRequest("http://" + VegTentWater.ip + "/cm?cmnd=POWER%20" + power, myapp.updateScreen);
