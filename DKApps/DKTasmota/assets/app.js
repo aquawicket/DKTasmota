e99164fc0b01408b23370cb38a7edf22fc38633b
dkuser <dkuser@digitalknob.com>
2021-05-31 03:10:38 -0700

    commit from git

--- DKApps/DKTasmota/assets/app.js
+++ DKApps/DKTasmota/assets/app.js
@@ -1,12 +1,12 @@
 var USE_CEF = 1;
 //var url = "http://www.google.com/";
 //var url = "chrome://gpu";
-var url = "http://127.0.0.1:2393/index.php";
+var url = "http://127.0.0.1:2393/dktasmota.html";
 
 var width = 800;
 var height = 600;
 CPP_DK_Create("DKCef,Cef,0,0,"+width+","+height+","+url);
-//CPP_DKCef_NewBrowser("Cef",0,0,width,height,url);
+CPP_DKCef_NewBrowser("Cef",0,0,width,height,url);
 
 
 /*
