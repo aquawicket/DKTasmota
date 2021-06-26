903a2fc626740d2ff57d768345ee24c38e95175d
dkuser <dkuser@digitalknob.com>
2021-06-26 06:15:34 -0700

    commit from git

--- DKApps/DKTasmota/assets/app.js
+++ DKApps/DKTasmota/assets/app.js
@@ -21,11 +21,30 @@ const height = 600
 CPP_DK_Create("DKCef,Cef,0,0,"+width+","+height+","+url)
 CPP_DKCef_NewBrowser("Cef",0,0,width,height,url)
 //CPP_DKCef_ShowDevTools(0)
-
+window.addEventListener("keydown", function mykeydown(event){
+	if(event.key === "F12")
+		CPP_DKCef_ShowDevTools(0)	
+}
 
 
 window.addEventListener("keydown", function mykeydown(event){
-	console.log("keydown: "+event.code)
+	console.log("app.js: window.onkeydown() ")
+	console.log("event.type: "+event.type)
+	console.log("event.altKey: "+event.altKey)
+	console.log("event.char: "+event.char)     //OBSOLETE
+	console.log("event.charCode: "+event.charCode)
+	console.log("event.code: "+event.code)
+	console.log("event.ctrlKey: "+event.ctrlKey)
+	console.log("event.isCOmposition: "+event.isComposision)
+	console.log("event.key: "+event.key)
+	console.log("event.keyCode: "+event.keyCode)
+	console.log("event.keyIdentifier: "+event.keyIdentifier)
+	console.log("event.locale: "+event.locale)
+	console.log("event.location: "+event.location)
+	console.log("event.metaKey: "+event.metaKey)
+	console.log("event.repeat: "+event.repeat)
+	console.log("event.shiftKey: "+event.shiftKey)
+	console.log("event.which: "+event.which)
 })
 
 window.addEventListener("mousedown", function mymousedowndown(){
